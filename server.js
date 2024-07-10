bdad857af004d7df60f65c99147cc850701f7cb6
parthamk <partha0988@gmail.com>
2024-07-10 08:21:49 +0530

    containerized

--- server.js
+++ server.js
@@ -8,9 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
+
+const port = process.env.PORT || 5000;
+
 app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
+// console.log(process.env.EMAIL_USER);
+// console.log(process.env.EMAIL_PASS);
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
